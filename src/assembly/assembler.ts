7e582c8e0e75bfbe2127b5cf112b7bc767863a28
Grigorije Jakisic <grigorije@hive.co.rs>
2026-05-17 11:33:11 +0200

    claudex/auto: edit

--- src/assembly/assembler.ts
+++ src/assembly/assembler.ts
@@ -36,6 +36,7 @@ import {
   renderExperienceWarnings as formatExperienceWarningsSection,
   formatCodebaseContextSection,
 } from './sections.js';
+import { formatProvenanceChainSection } from './sections/links.js';
 import { getLatestHighlights } from '../intelligence/session-highlights.js';
 import {
   findMatchingPatterns,
