1f113e9d9535ffc545866f4ad416e555fdca0a7a
Grigorije Jakisic <grigorije@hive.co.rs>
2026-05-18 16:01:33 +0200

    claudex/auto: session-start

---
+++ .tmp-backfill-term-entry.mjs
@@ -0,0 +1,4 @@
+
+import { recordSessionTermination, readLastTurnTexts } from './src/core/session-termination.js';
+import { runMigrations } from './src/core/migrations.js';
+export { recordSessionTermination, readLastTurnTexts, runMigrations };
